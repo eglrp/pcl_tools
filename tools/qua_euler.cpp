25df0dbe4647291f13fa39507262510c8fd8373c
glmhit <glmhit@126.com>
2018-03-12 16:41:39 +0800

    四元素的基本运算

--- tools/qua_euler.cpp
+++ tools/qua_euler.cpp
@@ -32,12 +32,41 @@ int main(int argc, char **argv)
   euler = trans.rotation().eulerAngles(2,1,0);  // yaw pitch roll
   std::cout << "pose  = "  << euler*57.3<< std::endl;
 
+
+    //From Euler to Quaternion:
+  Eigen::Quaterniond q0(Eigen::AngleAxisd(pcl::deg2rad((double)0.0), Eigen::Vector3d::UnitZ()));
+  q0.normalize();
+  euler = q0.toRotationMatrix().eulerAngles(2,1,0);
+  std::cout << "q0 euler  = \n"  << euler*57.3<< std::endl;
+
   //From Euler to Quaternion:
-  q = Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX())
-      * Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY())
-      * Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
-  std::cout << "Quaternion" << std::endl << q.coeffs() << std::endl;
+  Eigen::Quaterniond q1(Eigen::AngleAxisd(pcl::deg2rad((double)60.0), Eigen::Vector3d::UnitZ()));
+  q1.normalize();
+    //From Euler to Quaternion:
+  Eigen::Quaterniond p = Eigen::Quaterniond(0, 1, 1, -3);  // 空间点（1,1，,3）的坐标变换 , w=0，纯四元素表示坐标
+  Eigen::Quaterniond p_new = q1 * p * q1.inverse();
+  std::cout << p_new.vec() << std::endl;
+  std::cout << (q1.inverse() * p_new * q1).vec()<< std::endl;
+
+
+  Eigen::Quaterniond q2(Eigen::AngleAxisd(pcl::deg2rad((double)70.0), Eigen::Vector3d::UnitZ()));
+  q2.normalize();
+  //std::cout << "q_d2" << std::endl << q_d2.coeffs() << std::endl;
+
+  //angular difference
+  Eigen::Quaterniond q_diff = q1.inverse() * q2;
+  euler = q_diff.toRotationMatrix().eulerAngles(2,1,0);
+  std::cout << "q_diff euler  = \n"  << euler*57.3<< std::endl;
+ 
+
+ //angular sum
+  Eigen::Quaterniond q_sum = q1 * q2;
+  euler = q_sum.toRotationMatrix().eulerAngles(2,1,0);
+  std::cout << "q_sum euler  = \n"  << euler*57.3<< std::endl;
   
+  //q3.normalize();
+  //euler = q3.toRotationMatrix().eulerAngles(2,1,0);
+  //std::cout << "q3 euler  = \n"  << euler*57.3<< std::endl;
   
 
 
